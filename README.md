0f15177afa8f61288da568a157521b34d884cfe0
Mahmut Uludag <mahmut.uludag@kaust.edu.sa>
2016-09-08 16:55:00 +0300

    sample BLAST outputs and script to generate

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ your results, and select subsets easily for further analysis.
 * If you do not have any BLAST results in json format and if you first want to
   see how HSPs-db is working then you can use the sample results we
   have in the testdb folder. Call `index-folder.sh` script with argument `./testdb`
-  followed by the index name you choose earlier and your Elasticsearch server URL
+  followed by the index name you choose earlier, and URL of your Elasticsearch server 
 
 * If you already have outputs in BLAST archive format you can use
   `blast_formatter` command to regenerate your outputs in json format
@@ -41,7 +41,7 @@ your results, and select subsets easily for further analysis.
 * We want to have scripts that maps existing XML outputs to json format
   but this has not been done yet
 
-* For your new BLAST jobs it is best to produce outputs first in BLAST archive format
+* For your new BLAST searches it is best to produce outputs first in BLAST archive format
   then use `blast_formatter` to generate the outputs you normally read
   and the json output for indexing
 
---
+++ scripts/generate-testdb.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+# Generate sample outputs by submitting BLAST searches on NCBI servers
+# and index the results on the given Elasticsearch server.
+#
+# Set your email address and Elasticsearch server URL
+# using the configuration-variables before calling this script.
+#
+# Make sure you have Node.js installed
+# and you executed 'npm install' from project root folder.
+#
+# Call this script from project root folder, it executes 2 scripts
+# from the scripts folder and assumes it was executed from project root folder.
+# Do not forget to initialize your Elasticsearch index before running this script, see README
+
+import os
+
+# Configuration variables
+qseqids = ["sp|P05067|A4_HUMAN", "sp|P10636|TAU_HUMAN"]
+index = 'testdb'
+email = 'email@required.for.NCBI.search'
+server = 'http://localhost:9200'
+outfolder = './testdb'
+
+
+def submit_search(qseqids, email, database, program, outfile):
+    cmd = "node ./scripts/ncbi-sss-client.js --qseqids='{0}'" \
+          " --email='{1}'" \
+          " --database='{2}'" \
+          " --program='{3}'" \
+          " --expect=1" \
+          " --hitlist_size=10" \
+          " --alignments=10" \
+          " --word_size=6" \
+          " --filter=L" \
+          " --gapcosts='11 2'" \
+          " --outfile={4}/{5}"
+    cmd = cmd.format(qseqids, email, database, program, outfolder, outfile)
+    print('BLAST search: ' + cmd)
+    os.system(cmd)
+    print('BLAST search returned')
+
+
+def index_resultfile(outfile, id):
+    cmd = "./scripts/index.sh {0}/{1}.json {2} {3} {4}"
+    cmd = cmd.format(outfolder, outfile, id, index, server)
+    os.system(cmd)
+    print('index call returned: ' + cmd)
+
+
+def search_then_index(qseqids, email, database, program, outfile):
+    i = 0
+    for qseqid in qseqids:
+        i += 1
+        outf = outfile + str(i)
+        submit_search(qseqid, email, database, program, outf)
+        index_resultfile(outf, outf)
+
+
+# Search PDB protein database
+search_then_index(qseqids, email, 'pdbaa', 'blastp', 'pdbaa_search')
+exit()
+
+# Search Proteins from WGS metagenomic projects using blastp
+search_then_index(qseqids, email, 'env_nr', 'blastp', 'env_nr_search')
+
+# Since queries are from swissprot this may not be a good search
+# Search Uniprot KB using blastp
+search_then_index(qseqids, email, 'swissprot', 'blastp', 'uniprot_search')
+
+# Search refseq_representative_genomes using tblastn
+search_then_index(qseqids, email, 'refseq_representative_genomes',
+                  'tblastn', 'rg_search')
+
+# Search unfinished High Throughput Genomic Sequences; phases 0, 1 and 2
+search_then_index(qseqids, email, 'htgs', 'tblastn', 'htgs_search')
