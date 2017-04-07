054189be9065ba9dab469b83032d5efcf08c8f63
Mahmut Uludag <mahmut.uludag@kaust.edu.sa>
2017-04-07 15:17:29 +0300

    Elasticsearch username/password support

--- scripts/init-index.sh
+++ scripts/init-index.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Initialize Elasticsearch index with given name
 # for indexing sequence similarity search results
-# either in BLAST xml2 format or in widely used SAM format
+# both in BLAST xml2 format and in widely used SAM format
 # Example usage:
-# ./scripts/init-index.sh project-a http://localhost:9200/
+# ./scripts/init-index.sh hspsdb http://localhost:9200/
+# If your Elasticsearch server requires username and password to connect
+# then provide them as the 3rd option, e.g. [--user foo:bar]
 
 if [ $# -lt 1 ]
 then
@@ -14,15 +16,16 @@ fi
 
 index=${1-'hspsdb-test'}
 server=${2-'http://localhost:9200'}
+usernamepasswd=${3-'--user elastic:changeme'}
 
 echo "Deleting any existing index with name '${index}', ignore index_not_found_exception";
-curl -XDELETE "${server}/${index}/";
+curl -XDELETE "${server}/${index}/" ${usernamepasswd};
 echo;
 echo "Refreshing indicies";
-curl -XPOST "${server}/_refresh";
+curl -XPOST "${server}/_refresh" ${usernamepasswd};
 echo;
 echo "Initializing new index with name '${index}'";
-curl -XPUT "${server}/${index}/" -d '
+curl -XPUT "${server}/${index}/" ${usernamepasswd} -d '
 {
     "settings" : {
         "number_of_shards" : 1,
@@ -31,7 +34,7 @@ curl -XPUT "${server}/${index}/" -d '
     }
 }';
 
-es_5=`curl ${server} |& grep 'number" : "5' | wc -l`
+es_5=`curl ${server} ${usernamepasswd} 2>1 | grep 'number" : "5' | wc -l`
 
 if [ "$es_5" = "1" ]; then
     xml2mappings="./scripts/mappings-es5.json"
@@ -42,14 +45,14 @@ fi
 echo;
 echo "Setting index type mappings";
 type=xml2;
-curl -XPUT "${server}/${index}/_mapping/${type}"\
+curl -XPUT "${server}/${index}/_mapping/${type}" ${usernamepasswd}\
  -d  @${xml2mappings};
 type=sam;
-curl -XPUT "${server}/${index}/_mapping/${type}"\
+curl -XPUT "${server}/${index}/_mapping/${type}" ${usernamepasswd}\
  -d  @./scripts/mappings-sam.json;
 
 echo;
 echo "Refreshing indicies";
-curl -XPOST "${server}/${index}/_refresh";
+curl -XPOST "${server}/${index}/_refresh" ${usernamepasswd};
 
 echo;echo;
